55df000e454d19e9f10377cca99f8ee41ee59112
sungchulyun <proattacker@naver.com>
2024-05-13 23:31:21 +0900

    Mission 컨트롤러, 레포지토리 추가

---
+++ src/main/java/dailymissionproject/demo/repository/MissionRepository.java
@@ -0,0 +1,17 @@
+package dailymissionproject.demo.repository;
+
+import dailymissionproject.demo.entity.Mission;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MissionRepository {
+
+    private final EntityManager em;
+
+    public void save(Mission mission){
+        em.persist(mission);
+    }
+}
