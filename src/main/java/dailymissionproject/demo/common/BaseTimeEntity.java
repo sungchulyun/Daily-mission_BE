40c71c88b4a4fe16a8b09e4ce00e800f96123df2
sungchulyun <proattacker@naver.com>
2024-06-06 19:58:57 +0900

    feat : Created, Updated 시간 칼럼 추가

--- src/main/java/dailymissionproject/demo/common/BaseTimeEntity.java
+++ src/main/java/dailymissionproject/demo/common/BaseTimeEntity.java
@@ -1,6 +1,5 @@
 package dailymissionproject.demo.common;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
