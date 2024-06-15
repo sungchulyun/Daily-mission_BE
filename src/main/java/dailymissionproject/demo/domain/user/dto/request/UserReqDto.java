032db2ce0d3a2e935beb9862be5f618debe0c530
sungchulyun <proattacker@naver.com>
2024-06-15 14:40:56 +0900

    refactor : 최초 회원가입 DTO 수정

--- src/main/java/dailymissionproject/demo/domain/user/dto/request/UserReqDto.java
+++ src/main/java/dailymissionproject/demo/domain/user/dto/request/UserReqDto.java
@@ -4,25 +4,21 @@ import dailymissionproject.demo.domain.user.repository.User;
 import lombok.Builder;
 import lombok.Getter;
 @Getter
-
 public class UserReqDto {
 
     private final String name;
     private final String email;
-    private final String picture;
 
     @Builder
-    public UserReqDto(String name, String email, String picture){
+    public UserReqDto(String name, String email){
         this.name = name;
         this.email = email;
-        this.picture = picture;
     }
     @Builder
     public User toEntity(UserReqDto userReqDto){
         return User.builder()
                 .name(userReqDto.getName())
                 .email(userReqDto.getEmail())
-                .picture(userReqDto.getPicture())
                 .build();
     }
 }
