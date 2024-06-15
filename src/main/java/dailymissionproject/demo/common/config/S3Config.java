60ee58af6709aec7f6201cece5f042da933ece93
sungchulyun <proattacker@naver.com>
2024-06-15 14:40:28 +0900

    refactor : 이미지 변수 필드 수정

--- src/main/java/dailymissionproject/demo/common/config/S3Config.java
+++ src/main/java/dailymissionproject/demo/common/config/S3Config.java
@@ -7,18 +7,26 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.accessKey}")
 
     private String accessKey;
 
-    @Value("$cloud.aws.credentials.secret_key}")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
-    @Value("{cloud.aws.region.static}")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
@@ -30,4 +38,36 @@ public class S3Config {
                 .build();
     }
 
+    public String upload(MultipartFile file, String dirName)throws IOException {
+
+        File uploadFile = convert(file).orElseThrow(() -> new IllegalStateException("MultipartFile -> File로 전환 실패했습니다."));
+
+        String originalFileName = file.getOriginalFilename();
+        String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+
+        return "업로드 성공";
+    }
+
+    private Optional<File> convert(MultipartFile file)throws IOException {
+        
+        File convertFile = new File(genSerialNumber() + "_" + file.getOriginalFilename());
+        if(convertFile.createNewFile()){
+            try(FileOutputStream fos = new FileOutputStream(convertFile)){
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+        return Optional.empty();
+    }
+
+    private String genSerialNumber() {
+        LocalDateTime now = LocalDateTime.now();
+        String serial = "" + new DecimalFormat("0000").format(now.getYear())
+                + new DecimalFormat("00").format(now.getMonthValue())
+                + new DecimalFormat("00").format(now.getDayOfMonth())
+                + new DecimalFormat("00").format(now.getHour())
+                + new DecimalFormat("00").format(now.getMinute());
+
+        return serial;
+    }
 }
