a80d3c39d05d8fdf58f27a8aab9cb23e0c357172
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-06-19 17:41:29 +0900

    feat : 미션 상세조회 DTO 필드 수정

--- src/main/java/dailymissionproject/demo/domain/mission/dto/response/MissionResponseDto.java
+++ src/main/java/dailymissionproject/demo/domain/mission/dto/response/MissionResponseDto.java
@@ -7,11 +7,12 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 
 @Getter
-public class MissionSaveResponseDto {
+public class MissionResponseDto {
 
     private String title;
     private String content;
     private String imgUrl;
+    private String userName;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
@@ -19,7 +20,7 @@ public class MissionSaveResponseDto {
     private LocalDate endDate;
 
     @Builder
-    public MissionSaveResponseDto(String title, String content, String imgUrl, LocalDate startDate, LocalDate endDate){
+    public MissionResponseDto(String title, String content, String imgUrl, String userName, LocalDate startDate, LocalDate endDate){
         this.title = title;
         this.content = content;
         this.imgUrl = imgUrl;
