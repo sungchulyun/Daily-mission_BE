3ed9951a32a479a5050e9e9f7324dbc4ad9a5dfc
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-06-21 14:43:06 +0900

    refactor : Mission 생성자에 참여 암호키 필드 추가

--- src/main/java/dailymissionproject/demo/domain/mission/Service/MissionService.java
+++ src/main/java/dailymissionproject/demo/domain/mission/Service/MissionService.java
@@ -1,9 +1,7 @@
 package dailymissionproject.demo.domain.mission.Service;
 
 import dailymissionproject.demo.domain.mission.dto.request.MissionSaveRequestDto;
-import dailymissionproject.demo.domain.mission.dto.response.MissionHotListResponseDto;
-import dailymissionproject.demo.domain.mission.dto.response.MissionResponseDto;
-import dailymissionproject.demo.domain.mission.dto.response.MissionSaveResponseDto;
+import dailymissionproject.demo.domain.mission.dto.response.*;
 import dailymissionproject.demo.domain.mission.repository.Mission;
 import dailymissionproject.demo.domain.mission.repository.MissionRepository;
 import dailymissionproject.demo.domain.user.repository.User;
@@ -23,6 +21,7 @@ public class MissionService {
     private final UserRepository userRepository;
 
     //==미션 상세 조회==//
+    @Transactional(readOnly = true)
     public MissionResponseDto findById(Long id){
         Mission mission = missionRepository.findById(id).orElseThrow(()-> new NoSuchElementException("해당 미션이 존재하지 않습니다."));
 
@@ -39,6 +38,7 @@ public class MissionService {
 
 
     //== 미션 생성 ==//
+    @Transactional
     public MissionSaveResponseDto save(String userName, MissionSaveRequestDto missionReqDto){
 
         User findUser = userRepository.findOneByName(userName);
@@ -46,14 +46,16 @@ public class MissionService {
             throw new RuntimeException("존재하지 않는 사용자 닉네임입니다.");
         }
 
+        String credential = String.valueOf(UUID.randomUUID());
+
         Mission mission = Mission.builder()
                         .title(missionReqDto.getTitle())
                         .content(missionReqDto.getContent())
                         .imageUrl(missionReqDto.getImgUrl())
+                        .credential(credential)
                         .build();
         mission.setUser(findUser);
         missionRepository.save(mission);
-        String credential = String.valueOf(UUID.randomUUID());
 
         MissionSaveResponseDto responseDto = MissionSaveResponseDto.builder()
                 .credential(credential).build();
@@ -84,8 +86,44 @@ public class MissionService {
     }
 
     //New 미션 불러오기 ==//
+    @Transactional(readOnly = true)
+    public List<MissionNewListResponseDto> findNewList(){
+
+        List<MissionNewListResponseDto> res = new ArrayList<>();
+
+        List<Mission> newLists = missionRepository.findAllByParticipantSize();
+        for(Mission mission : newLists){
+            MissionNewListResponseDto hotMission = MissionNewListResponseDto.builder()
+                    .title(mission.getTitle())
+                    .content(mission.getContent())
+                    .userName(mission.getUser().getName())
+                    .startDate(mission.getStartDate())
+                    .endDate(mission.getEndDate())
+                    .build();
+
+            res.add(hotMission);
+        }
+        return res;
+    }
 
     //모든 미션 불러오기 ==//
+    @Transactional(readOnly = true)
+    public List<MissionAllListResponseDto> findAllList(){
 
+        List<MissionAllListResponseDto> res = new ArrayList<>();
 
+        List<Mission> allLists = missionRepository.findAllByParticipantSize();
+        for(Mission mission : allLists){
+            MissionAllListResponseDto hotMission = MissionAllListResponseDto.builder()
+                    .title(mission.getTitle())
+                    .content(mission.getContent())
+                    .userName(mission.getUser().getName())
+                    .startDate(mission.getStartDate())
+                    .endDate(mission.getEndDate())
+                    .build();
+
+            res.add(hotMission);
+        }
+        return res;
+    }
 }
