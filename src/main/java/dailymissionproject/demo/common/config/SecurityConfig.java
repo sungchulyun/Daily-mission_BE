7d934690c8d611ce2836595ae18cd343b55d8212
sungchulyun <proattacker@naver.com>
2024-07-04 22:13:58 +0900

    refactor : 권한 검증 @EnableMethodSecurity 추가

--- src/main/java/dailymissionproject/demo/common/config/SecurityConfig.java
+++ src/main/java/dailymissionproject/demo/common/config/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.web.SecurityFilterChain;
 //import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,17 +32,13 @@ import java.util.Collections;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomSuccessHandler customSuccessHandler;
     private final JWTUtil jwtUtil;
 
-    //private final CustomOAuth2UserService customOAuth2UserService;
-
-    //public SecurityConfig(CustomOAuth2UserService customOAuth2UserService){
-    //    this.customOAuth2UserService = customOAuth2UserService;
-    //}
     @Bean
     public SecurityFilterChain config(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception{
 
@@ -100,25 +98,5 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
-
-        /*
-        http
-
-               .csrf(AbstractHttpConfigurer::disable)
-               .sessionManagement((sessionManagement) ->
-                       sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                       )
-               .formLogin(AbstractHttpConfigurer::disable)
-               .httpBasic(AbstractHttpConfigurer::disable)
-               .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                       .requestMatchers(new MvcRequestMatcher(introspector, "/")).permitAll()
-               )
-               .oauth2Login(oauth2Login ->
-                       oauth2Login.userInfoEndpoint(userInfoEndpointConfig ->
-                       userInfoEndpointConfig.userService(customOAuth2UserService)))
-               ;
-       return http.build();
-
-        */
     }
 }
