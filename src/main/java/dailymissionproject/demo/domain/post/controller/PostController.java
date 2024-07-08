4627ef8c95b89fee98334012afd100c9a283a6e3
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-07-08 14:49:58 +0900

    feat : 조회 결과값을 cacheing으로 수정

--- src/main/java/dailymissionproject/demo/domain/post/controller/PostController.java
+++ src/main/java/dailymissionproject/demo/domain/post/controller/PostController.java
@@ -6,6 +6,10 @@ import dailymissionproject.demo.domain.post.dto.request.PostUpdateRequestDto;
 import dailymissionproject.demo.domain.post.dto.response.PostResponseDto;
 import dailymissionproject.demo.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/post")
 @RequiredArgsConstructor
+@Slf4j
 public class PostController {
 
     private final PostService postService;
@@ -23,29 +28,41 @@ public class PostController {
     //== 인증 글 생성==//
     @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping("/create")
+    @Caching(evict = {
+            //전체 포스트
+            @CacheEvict(value = "postLists", key = "'all'"),
+            @CacheEvict(value = "postLists", key = "'user-' + #username" ),
+            @CacheEvict(value = "postLists", key = "'mission-' + #requestDto.missionId"),
+    })
     public Long save(@RequestBody PostSaveRequestDto requestDto) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         CustomOAuth2User user = (CustomOAuth2User) authentication.getPrincipal();
+        String username = user.getUsername();
         return postService.save(user.getUsername(), requestDto);
     }
 
     //== 인증 글 상세 조회==//
-    @PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasAnyRole('USER')")
     @GetMapping("/getInfo/{id}")
+    @Cacheable(value = "posts", key = "#id")
     public PostResponseDto findById(@PathVariable("id") Long id){
         return postService.findById(id);
     }
 
     //== 유저별 전체 포스트 목록 불러오기==//
-    @PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_GUEST)")
     @GetMapping("/getUser/{id}")
+    @Cacheable(value = "postLists", key = "'user-' + #username")
     public List<PostResponseDto> findByUser(@PathVariable("id") Long id){
-        return postService.findAllByUser(id);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomOAuth2User user = (CustomOAuth2User) authentication.getPrincipal();
+        String username = user.getUsername();
+        return postService.findAllByUser(username);
     }
 
-    //== 유저별 전체 포스트 목록 불러오기==//
-    @PreAuthorize("hasRole('ROLE_USER)")
+    //== 미션별 전체 포스트 목록 불러오기==//
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("getMission/{id}")
     public List<PostResponseDto> findByMission(@PathVariable("id") Long id){
         return postService.findAllByMission(id);
