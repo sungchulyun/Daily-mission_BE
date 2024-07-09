ccea7dfcbb34ece59b92257bfadc34b165308f45
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-07-09 11:02:08 +0900

    refactor : 캐시 설정 추가 및 엔드포인트별 인가 설정 추가

--- src/main/java/dailymissionproject/demo/domain/participant/controller/ParticipantController.java
+++ src/main/java/dailymissionproject/demo/domain/participant/controller/ParticipantController.java
@@ -1,9 +1,11 @@
 package dailymissionproject.demo.domain.participant.controller;
 
+import dailymissionproject.demo.domain.auth.dto.CustomOAuth2User;
 import dailymissionproject.demo.domain.participant.dto.request.ParticipantSaveRequestDto;
 import dailymissionproject.demo.domain.participant.service.ParticipantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,9 +16,9 @@ public class ParticipantController {
     private final ParticipantService participantService;
 
     //== 미션 참여==//
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PostMapping("/join/{userName}")
-    public boolean save(@PathVariable("userName")String userName, @RequestBody ParticipantSaveRequestDto requestDto){
-        return participantService.save(userName, requestDto);
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @PostMapping("/join")
+    public boolean save(@AuthenticationPrincipal CustomOAuth2User user, @RequestBody ParticipantSaveRequestDto requestDto){
+        return participantService.save(user.getUsername(), requestDto);
     }
 }
