a490aa57f23154326253f6d7fe67783ce265ba55
sungchulyun <proattacker@naver.com>
2024-08-19 23:14:40 +0900

    fix: 미션 엔티티 @JsonForwardReference 추가

--- src/main/java/dailymissionproject/demo/domain/mission/repository/Mission.java
+++ src/main/java/dailymissionproject/demo/domain/mission/repository/Mission.java
@@ -1,20 +1,21 @@
 package dailymissionproject.demo.domain.mission.repository;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import dailymissionproject.demo.common.repository.BaseTimeEntity;
 import dailymissionproject.demo.domain.mission.exception.MissionException;
 import dailymissionproject.demo.domain.missionRule.dto.DateDto;
 import dailymissionproject.demo.domain.missionRule.repository.MissionRule;
 import dailymissionproject.demo.domain.missionRule.repository.Week;
-import dailymissionproject.demo.domain.participant.repository.Participant;
 import dailymissionproject.demo.domain.participant.dto.response.ParticipantUserDto;
+import dailymissionproject.demo.domain.participant.repository.Participant;
 import dailymissionproject.demo.domain.post.repository.Post;
 import dailymissionproject.demo.domain.user.repository.User;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +40,7 @@ public class Mission extends BaseTimeEntity {
     private User user;
 
     @OneToMany(mappedBy = "mission")
+    @JsonManagedReference
     private List<Participant> participants = new ArrayList<>();
 
     @OneToMany(mappedBy = "mission")
