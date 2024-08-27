6846d30294bb79bc009aa294966408fa19014502
sungchulyun <proattacker@naver.com>
2024-08-27 22:44:48 +0900

    test : ApplicationTest 메서드 주석처리

--- src/test/java/dailymissionproject/demo/DemoApplicationTests.java
+++ src/test/java/dailymissionproject/demo/DemoApplicationTests.java
@@ -1,15 +1,10 @@
 package dailymissionproject.demo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ContextConfiguration(classes = DemoApplication.class)
 class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
