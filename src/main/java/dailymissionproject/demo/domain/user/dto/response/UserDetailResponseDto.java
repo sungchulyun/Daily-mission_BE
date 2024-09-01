03ed5e50ef3dab05b6dab3cdfee2095eb20ecf23
sungchulyun <proattacker@naver.com>
2024-09-02 03:49:08 +0900

    refactor : DTO 클래스명 수정, 불변객체 전환

--- src/main/java/dailymissionproject/demo/domain/user/dto/response/UserDetailResponseDto.java
+++ src/main/java/dailymissionproject/demo/domain/user/dto/response/UserDetailResponseDto.java
@@ -6,20 +6,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
 @Schema(description = "유저 정보 응답 DTO")
-public class UserResDto {
+public class UserDetailResponseDto {
 
     @Schema(description = "유저 닉네임")
-    private  String name;
+    private final String userNickname;
     @Schema(description = "유저 이메일")
-    private  String email;
+    private final String email;
     @Schema(description = "유저 이미지 썸네일")
-    private  String imageUrl;
+    private final String imageUrl;
 
     @Builder
-    UserResDto(String name, String email, String imageUrl){
-        this.name = name;
+    public UserDetailResponseDto(String userNickname, String email, String imageUrl) {
+        this.userNickname = userNickname;
         this.email = email;
         this.imageUrl = imageUrl;
     }
