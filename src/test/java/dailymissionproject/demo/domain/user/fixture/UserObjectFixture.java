0b4f548f213c531fa985b2a374b457f15a26138f
sungchulyun <proattacker@naver.com>
2024-09-24 00:40:52 +0900

    test : testFixture Mock 객체 추가

--- src/test/java/dailymissionproject/demo/domain/user/fixture/UserObjectFixture.java
+++ src/test/java/dailymissionproject/demo/domain/user/fixture/UserObjectFixture.java
@@ -1,14 +1,32 @@
 package dailymissionproject.demo.domain.user.fixture;
 
+import dailymissionproject.demo.domain.user.dto.request.UserUpdateRequestDto;
 import dailymissionproject.demo.domain.user.dto.response.UserDetailResponseDto;
+import dailymissionproject.demo.domain.user.dto.response.UserUpdateResponseDto;
 
 public class UserObjectFixture {
+
+
 
     public static UserDetailResponseDto getUserResponse(){
         return UserDetailResponseDto.builder()
                 .nickname("윤성철")
                 .email("proattacker641@gmail.com")
                 .imageUrl("https://aws-s3.jpg")
+                .build();
+    }
+
+    public static UserUpdateRequestDto getUserUpdateRequest(){
+        return UserUpdateRequestDto.builder()
+                .nickname("sungchul")
+                .build();
+    }
+
+    public static UserUpdateResponseDto getUserUpdateResponse(){
+        return UserUpdateResponseDto.builder()
+                .username("google 1923819273")
+                .nickname("sungchul")
+                .imageUrl("https://AWS-s3/modifedImages.jpg")
                 .build();
     }
 }
