1a30db5eaa53166fe314c4160c2226adf9030611
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-09-24 15:55:16 +0900

    test : stub User 객체 추가

--- src/test/java/dailymissionproject/demo/domain/user/fixture/UserObjectFixture.java
+++ src/test/java/dailymissionproject/demo/domain/user/fixture/UserObjectFixture.java
@@ -3,14 +3,24 @@ package dailymissionproject.demo.domain.user.fixture;
 import dailymissionproject.demo.domain.user.dto.request.UserUpdateRequestDto;
 import dailymissionproject.demo.domain.user.dto.response.UserDetailResponseDto;
 import dailymissionproject.demo.domain.user.dto.response.UserUpdateResponseDto;
+import dailymissionproject.demo.domain.user.repository.Role;
+import dailymissionproject.demo.domain.user.repository.User;
 
 public class UserObjectFixture {
 
-
-
+    public static User getUserFixture(){
+        return User.builder()
+                .username("google 1923819273")
+                .email("google@gmail.com")
+                .nickname("sungchul")
+                .imageUrl("https://aws-s3.jpg")
+                .name("윤성철")
+                .role(Role.USER)
+                .build();
+    }
     public static UserDetailResponseDto getUserResponse(){
         return UserDetailResponseDto.builder()
-                .nickname("윤성철")
+                .nickname("sungchul")
                 .email("proattacker641@gmail.com")
                 .imageUrl("https://aws-s3.jpg")
                 .build();
