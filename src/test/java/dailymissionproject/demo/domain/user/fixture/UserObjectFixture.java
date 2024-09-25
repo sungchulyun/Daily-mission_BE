69118591725f931c842636bdaa5a169349f9c6b3
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-09-25 15:26:47 +0900

    test : User Fixture 주석 추가

--- src/test/java/dailymissionproject/demo/domain/user/fixture/UserObjectFixture.java
+++ src/test/java/dailymissionproject/demo/domain/user/fixture/UserObjectFixture.java
@@ -8,6 +8,10 @@ import dailymissionproject.demo.domain.user.repository.User;
 
 public class UserObjectFixture {
 
+    /**
+     * 유저 엔티티 fixture를 반환합니다.
+     * @return
+     */
     public static User getUserFixture(){
         return User.builder()
                 .username("google 1923819273")
@@ -18,6 +22,11 @@ public class UserObjectFixture {
                 .role(Role.USER)
                 .build();
     }
+
+    /**
+     * 유저 상세 응답 객체를 반환합니다.
+     * @return
+     */
     public static UserDetailResponseDto getUserResponse(){
         return UserDetailResponseDto.builder()
                 .nickname("sungchul")
@@ -26,12 +35,20 @@ public class UserObjectFixture {
                 .build();
     }
 
+    /**
+     * 유저 수정 요청 객체를 반환합니다.
+     * @return
+     */
     public static UserUpdateRequestDto getUserUpdateRequest(){
         return UserUpdateRequestDto.builder()
                 .nickname("sungchul")
                 .build();
     }
 
+    /**
+     * 유저 수정 응답 객체를 반환합니다.
+     * @return
+     */
     public static UserUpdateResponseDto getUserUpdateResponse(){
         return UserUpdateResponseDto.builder()
                 .username("google 1923819273")
