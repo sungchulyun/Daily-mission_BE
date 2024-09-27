920d4f86a948b659d488eaf9c9a720faa61c8515
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-09-27 13:19:20 +0900

    test : 미션 리스트 조회 단위 테스트 추

--- src/test/java/dailymissionproject/demo/domain/mission/controller/MissionControllerTest.java
+++ src/test/java/dailymissionproject/demo/domain/mission/controller/MissionControllerTest.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dailymissionproject.demo.domain.mission.dto.page.PageResponseDto;
 import dailymissionproject.demo.domain.mission.dto.request.MissionSaveRequestDto;
 import dailymissionproject.demo.domain.mission.dto.request.MissionUpdateRequestDto;
-import dailymissionproject.demo.domain.mission.dto.response.MissionDetailResponseDto;
-import dailymissionproject.demo.domain.mission.dto.response.MissionSaveResponseDto;
-import dailymissionproject.demo.domain.mission.dto.response.MissionUpdateResponseDto;
+import dailymissionproject.demo.domain.mission.dto.response.*;
 import dailymissionproject.demo.domain.mission.exception.MissionException;
 import dailymissionproject.demo.domain.mission.exception.MissionExceptionCode;
 import dailymissionproject.demo.domain.mission.fixture.MissionObjectFixture;
@@ -25,10 +23,13 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static dailymissionproject.demo.domain.mission.exception.MissionExceptionCode.MISSION_NOT_FOUND;
@@ -73,11 +74,13 @@ class MissionControllerTest {
     private final PageResponseDto newMissionListResponse = MissionObjectFixture.getNewMissionListResponse();
     private final PageResponseDto allMissionListResponse = MissionObjectFixture.getAllMissionListResponse();
 
+    private final Pageable pageable = PageRequest.of(0,3 );
     private final Long missionId = 1L;
 
     @Nested
     @DisplayName("미션 조회 컨트롤러 테스트")
     class MissionReadControllerTest {
+
 
         @Test
         @DisplayName("미션 상세정보를 조회할 수 있다.")
@@ -105,18 +108,123 @@ class MissionControllerTest {
             resultActions.andExpect(jsonPath("$.data.missionRuleResponseDto.week.sun").value(false));
             resultActions.andExpect(jsonPath("$.data.participantDto[0].id").value(missionId));
         }
-    }
+        @Test
+        @DisplayName("미션이 존재하지 않으면 상세 조회에 실패한다.")
+        void detail_read_mission_fail_when_mission_not_exits() throws Exception {
 
-    @Test
-    @DisplayName("미션이 존재하지 않으면 상세 조회에 실패한다.")
-    void detail_read_mission_fail_when_mission_not_exits() throws Exception {
+            when(missionService.findById(any()))
+                    .thenThrow(new MissionException(MISSION_NOT_FOUND));
 
-        when(missionService.findById(any()))
-                .thenThrow(new MissionException(MISSION_NOT_FOUND));
+            mockMvc.perform(get("/api/v1/mission/{missionId}", missionId)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .with(csrf()))
+                    .andExpect(status().isBadRequest());
+        }
 
-        mockMvc.perform(get("/api/v1/mission/{missionId}", missionId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .with(csrf()))
-                .andExpect(status().isBadRequest());
+        @Test
+        @DisplayName("인기 미션 리스트를 조회할 수 있다.")
+        void hot_mission_read_list_success() throws Exception {
+            MissionHotListResponseDto hotMission_1 = MissionHotListResponseDto.builder()
+                    .id(1L)
+                    .title("미션1")
+                    .content("열심히 합니다.")
+                    .imageUrl("THUMBNAIL1.jpg")
+                    .nickname("yoonsu")
+                    .startDate(LocalDate.now().minusDays(10))
+                    .endDate(LocalDate.now().plusDays(10))
+                    .build();
+
+            when(missionService.findHotList(any())).thenReturn(hotMissionListResponse);
+
+            ResultActions resultActions = mockMvc.perform(get("/api/v1/mission/hot")
+                    .param("page", String.valueOf(pageable.getPageNumber()))
+                    .param("size", String.valueOf(pageable.getPageSize()))
+                    .with(csrf())
+            )
+                    .andExpect(status().isOk())
+                    .andDo(print());
+
+            resultActions.andExpect(jsonPath("$.success").value(true));
+            resultActions.andExpect(jsonPath("$.code").value(200));
+            resultActions.andExpect(jsonPath("$.data[0].id").value(hotMission_1.getId()));
+            resultActions.andExpect(jsonPath("$.data[0].title").value(hotMission_1.getTitle()));
+            resultActions.andExpect(jsonPath("$.data[0].content").value(hotMission_1.getContent()));
+            resultActions.andExpect(jsonPath("$.data[0].imageUrl").value(hotMission_1.getImageUrl()));
+            resultActions.andExpect(jsonPath("$.data[0].nickname").value(hotMission_1.getNickname()));
+            resultActions.andExpect(jsonPath("$.data[0].startDate").value(hotMission_1.getStartDate().toString()));
+            resultActions.andExpect(jsonPath("$.data[0].endDate").value(hotMission_1.getEndDate().toString()));
+        }
+
+        @Test
+        @DisplayName("신규 미션 리스트를 조회할 수 있다.")
+        void new_mission_read_list_success() throws Exception {
+            MissionNewListResponseDto newMission_1 = MissionNewListResponseDto.builder()
+                    .id(1L)
+                    .title("미션1")
+                    .content("열심히 합니다.")
+                    .imageUrl("THUMBNAIL1.jpg")
+                    .nickname("yoonsu")
+                    .startDate(LocalDate.now().minusDays(10))
+                    .endDate(LocalDate.now().plusDays(10))
+                    .build();
+
+            //when
+            when(missionService.findNewList(any())).thenReturn(newMissionListResponse);
+
+            ResultActions resultActions = mockMvc.perform(get("/api/v1/mission/new")
+                    .param("page", String.valueOf(pageable.getPageNumber()))
+                    .param("size", String.valueOf(pageable.getPageSize()))
+                    .with(csrf()))
+                    .andExpect(status().isOk())
+                    .andDo(print());
+
+            //then
+            resultActions.andExpect(jsonPath("$.success").value(true));
+            resultActions.andExpect(jsonPath("$.code").value(200));
+            resultActions.andExpect(jsonPath("$.data[0].id").value(newMission_1.getId()));
+            resultActions.andExpect(jsonPath("$.data[0].title").value(newMission_1.getTitle()));
+            resultActions.andExpect(jsonPath("$.data[0].content").value(newMission_1.getContent()));
+            resultActions.andExpect(jsonPath("$.data[0].imageUrl").value(newMission_1.getImageUrl()));
+            resultActions.andExpect(jsonPath("$.data[0].nickname").value(newMission_1.getNickname()));
+            resultActions.andExpect(jsonPath("$.data[0].startDate").value(newMission_1.getStartDate().toString()));
+            resultActions.andExpect(jsonPath("$.data[0].endDate").value(newMission_1.getEndDate().toString()));
+        }
+
+        @Test
+        @DisplayName("전체 미션 리스트를 조회할 수 있다.")
+        void all_mission_read_list_success() throws Exception {
+            MissionAllListResponseDto allMission_1 = MissionAllListResponseDto.builder()
+                    .id(1L)
+                    .title("미션1")
+                    .content("열심히 합니다.")
+                    .imageUrl("THUMBNAIL1.jpg")
+                    .nickname("yoonsu")
+                    .startDate(LocalDate.now().minusDays(10))
+                    .endDate(LocalDate.now().plusDays(10))
+                    .build();
+            //when
+            when(missionService.findAllList(any())).thenReturn(allMissionListResponse);
+
+            ResultActions resultActions = mockMvc.perform(get("/api/v1/mission/all")
+                            .param("page", String.valueOf(pageable.getPageNumber()))
+                            .param("size", String.valueOf(pageable.getPageSize()))
+                            .with(csrf()))
+                    .andExpect(status().isOk())
+                    .andDo(print());
+
+            //then
+            resultActions.andExpect(jsonPath("$.success").value(true));
+            resultActions.andExpect(jsonPath("$.code").value(200));
+            resultActions.andExpect(jsonPath("$.data[0].id").value(allMission_1.getId()));
+            resultActions.andExpect(jsonPath("$.data[0].title").value(allMission_1.getTitle()));
+            resultActions.andExpect(jsonPath("$.data[0].content").value(allMission_1.getContent()));
+            resultActions.andExpect(jsonPath("$.data[0].imageUrl").value(allMission_1.getImageUrl()));
+            resultActions.andExpect(jsonPath("$.data[0].nickname").value(allMission_1.getNickname()));
+            resultActions.andExpect(jsonPath("$.data[0].startDate").value(allMission_1.getStartDate().toString()));
+            resultActions.andExpect(jsonPath("$.data[0].endDate").value(allMission_1.getEndDate().toString()));
+        }
+
     }
 }
+
+
