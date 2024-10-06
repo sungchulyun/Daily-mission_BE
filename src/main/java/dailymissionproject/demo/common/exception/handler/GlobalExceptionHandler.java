acbce9c9611a1028c39d4da38376295ddf50bcc7
sungchulyun <proattacker@naver.com>
2024-10-06 20:36:10 +0900

    refactor : @valid 예외 전략 추가

--- src/main/java/dailymissionproject/demo/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/dailymissionproject/demo/common/exception/handler/GlobalExceptionHandler.java
@@ -6,15 +6,19 @@ import dailymissionproject.demo.common.exception.AbstractCustomException;
 import dailymissionproject.demo.common.meta.MetaService;
 import dailymissionproject.demo.domain.auth.exception.AuthExceptionCode;
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.security.SignatureException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
 import java.util.Map;
 import java.util.Objects;
 
@@ -70,6 +74,23 @@ public class GlobalExceptionHandler {
 
         Map<String, String> message = Map.of(KEY_MESSAGE, errorMessage);
         return new ResponseEntity<>(GlobalResponse.error(HttpStatus.BAD_REQUEST.value(), message), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GlobalResponse> handleValidationException(MethodArgumentNotValidException exception){
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder sb = new StringBuilder();
+        for(FieldError fieldError : bindingResult.getFieldErrors()){
+            sb.append("[");
+            sb.append(fieldError.getField());
+            sb.append("] (은)는 ");
+            sb.append(fieldError.getDefaultMessage());
+            sb.append(" 입력된 값 : [");
+            sb.append(fieldError.getRejectedValue());
+            sb.append("]");
+        }
+        return new ResponseEntity<>(GlobalResponse.error(HttpStatus.BAD_REQUEST.value(), sb.toString()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {SignatureException.class, MalformedJwtException.class, ExpiredJwtException.class, UnsupportedJwtException.class, IllegalArgumentException.class})
