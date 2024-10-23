e145b0b87647573fcd6fbab99e8b08f5d857d6e1
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-10-23 14:36:07 +0900

    test : mariaDB container config 파일 추가

---
+++ src/test/java/dailymissionproject/demo/global/config/IntegrationTestSupport.java
@@ -0,0 +1,17 @@
+package dailymissionproject.demo.global.config;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class IntegrationTestSupport {
+    @Container
+    MariaDBContainer MARIADB_CONTAINER = new MariaDBContainer("mariadb:10.11");
+}
