9fe3aa7cdab4f43a83f921f587163a21b59a59b5
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-10-24 16:26:56 +0900

    test : mariaDB container config 파일 추가

--- src/test/java/dailymissionproject/demo/global/config/IntegrationTestSupport.java
+++ src/test/java/dailymissionproject/demo/global/config/IntegrationTestSupport.java
@@ -3,6 +3,8 @@ package dailymissionproject.demo.global.config;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -12,6 +14,15 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class IntegrationTestSupport {
+
     @Container
-    MariaDBContainer MARIADB_CONTAINER = new MariaDBContainer("mariadb:10.11");
+    public static MariaDBContainer<?> mariaDBContainer = new MariaDBContainer<>("mariadb:10.11")
+            .withReuse(false);
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> mariaDBContainer.getJdbcUrl());
+        registry.add("spring.datasource.username", () -> mariaDBContainer.getUsername());
+        registry.add("spring.datasource.password", () -> mariaDBContainer.getPassword());
+    }
 }
