75412fd3d4af2c53f906a3f8e2d4dba96a7dcd93
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-10-29 17:28:02 +0900

    feat : ImageController 추가

---
+++ src/main/java/dailymissionproject/demo/domain/image/controller/ImageController.java
@@ -0,0 +1,37 @@
+package dailymissionproject.demo.domain.image.controller;
+
+import dailymissionproject.demo.domain.image.service.ImageService;
+import dailymissionproject.demo.domain.mission.repository.Mission;
+import dailymissionproject.demo.domain.mission.repository.MissionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImageService imageService;
+    private final MissionRepository missionRepository;
+
+    @GetMapping("/presigned-url/get")
+    public ResponseEntity<?> getPresignedUrl(@RequestParam String fileName) {
+        String presignedUrl = String.valueOf(imageService.generateGetPresignedUrl(fileName));
+        return ResponseEntity.ok(presignedUrl);
+    }
+
+    @GetMapping("/presigned-url/put")
+    public ResponseEntity<String> getPutPresignedUrl(@RequestParam String fileName) {
+        String presignedUrl = String.valueOf(imageService.generatePostPresignedUrl(fileName));
+        return ResponseEntity.ok(presignedUrl);
+    }
+
+    @PostMapping("/save-image")
+    public ResponseEntity<?> saveImage(@RequestParam String imageUrl){
+        Mission mission = new Mission();
+        mission.setImageUrl(imageUrl);
+
+        missionRepository.save(mission);
+        return ResponseEntity.ok("Mission Thumbnail saved Successfully");
+    }
+}
