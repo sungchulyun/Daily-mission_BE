09bb4b7ae3dd47a6e5f31a34acc936d320aef0c6
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-08 17:59:27 +0900

    test : Participant Fixture 추가

---
+++ src/test/java/dailymissionproject/demo/domain/participant/fixture/ParticipantObjectFixture.java
@@ -0,0 +1,13 @@
+package dailymissionproject.demo.domain.participant.fixture;
+
+import dailymissionproject.demo.domain.participant.dto.request.ParticipantSaveRequestDto;
+
+public class ParticipantObjectFixture {
+
+    public static ParticipantSaveRequestDto getParticipantSaveRequest(){
+        return ParticipantSaveRequestDto.builder()
+                .credential("CREDENTIAL")
+                .missionId(1L)
+                .build();
+    }
+}
