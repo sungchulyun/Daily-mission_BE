2a823c53d772cbcd37699d7145beeb6f039dc42a
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-11 17:01:16 +0900

    test : 통합테스트 test container config 추가

--- src/test/java/dailymissionproject/demo/global/config/IntegrationTestSupport.java
+++ src/test/java/dailymissionproject/demo/global/config/IntegrationTestSupport.java
@@ -1,20 +1,48 @@
 package dailymissionproject.demo.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Tag("integration")
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class IntegrationTestSupport {
+    protected static final Logger log = LogManager.getLogger(IntegrationTestSupport.class);
+    protected static MariaDBContainer<?> MARIADB_CONTAINER;
 
+    static {
+        MARIADB_CONTAINER = new MariaDBContainer<>(DockerImageName.parse("mariadb:10.11"))
+                .withDatabaseName("test")
+                .withUsername("root")
+                .withPassword("root");
+
+                MARIADB_CONTAINER.start();
+    }
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+    @Autowired
+    protected MockMvc mockMvc;
+
+
+    /*
     @Container
     public static MariaDBContainer<?> mariaDBContainer = new MariaDBContainer<>("mariadb:10.11")
             .withReuse(false);
@@ -25,4 +53,6 @@ public class IntegrationTestSupport {
         registry.add("spring.datasource.username", () -> mariaDBContainer.getUsername());
         registry.add("spring.datasource.password", () -> mariaDBContainer.getPassword());
     }
+
+     */
 }
