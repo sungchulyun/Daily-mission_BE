8e7dda821a367b5acaa6f07b455c511dc2ff2030
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-11 10:08:22 +0900

    feat : 파일 업로드 권한이 부여된 preSignedUrl 생성 로직 추가

--- src/main/java/dailymissionproject/demo/domain/image/service/ImageService.java
+++ src/main/java/dailymissionproject/demo/domain/image/service/ImageService.java
@@ -2,11 +2,17 @@ package dailymissionproject.demo.domain.image.service;
 
 import dailymissionproject.demo.common.util.S3Util;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 import java.io.IOException;
+import java.net.URL;
 import java.text.DecimalFormat;
+import java.time.Duration;
 import java.util.Calendar;
 
 @Service
@@ -14,7 +20,10 @@ import java.util.Calendar;
 public class ImageService {
 
     private final S3Util s3Util;
+    private final S3Presigner presigner;
 
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
 
     /**
      * POST Thumbnail 디렉토리 생성
@@ -51,21 +60,25 @@ public class ImageService {
         return s3Util.upload(multipartFile, dirName);
     }
 
-    /*
-    public String uploadImg(MultipartFile file)throws IOException {
-        try {
-            String fileName = file.getOriginalFilename();
-            String fileUrl = bucketUrl + "/" + fileName;
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentType(file.getContentType());
-            metadata.setContentLength(file.getSize());
-            amazonS3.putObject(bucket, fileName, file.getInputStream(), metadata);
-            return fileUrl;
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("이미지 업로드에 실패했습니다.");
-        }
-    }
-
+    /**
+     * 파일 업로드를 위한 presignedUrl 생성 메서드
+     * @param fileName
+     * @param title
+     * @return
      */
+    public URL generatePostPresignedUrl(String fileName, String title){
+        String name = title + "/" + fileName + getPostDir();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(name)
+                .build();
+
+        PutObjectPresignRequest putObjectPresignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(5))
+                .putObjectRequest(putObjectRequest)
+                .build();
+
+        return presigner.presignPutObject(putObjectPresignRequest).url();
+    }
 }
