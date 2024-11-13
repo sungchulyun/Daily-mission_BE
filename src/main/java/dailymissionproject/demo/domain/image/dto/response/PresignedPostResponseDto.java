4483c19e40fbe66aaf8ca9ab763ef625d7d39e8e
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-13 14:12:37 +0900

    feat : swagger description 추가

--- src/main/java/dailymissionproject/demo/domain/image/dto/response/PresignedPostResponseDto.java
+++ src/main/java/dailymissionproject/demo/domain/image/dto/response/PresignedPostResponseDto.java
@@ -1,13 +1,18 @@
 package dailymissionproject.demo.domain.image.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(force = true)
+@Schema(description = "preSigned-url 응답 DTO")
 public class PresignedPostResponseDto {
+
+    @Schema(description = "presigned-url")
     private String url;
+    @Schema(description = "파일 접근 경로")
     private String path;
 
     @Builder
