119e585d05f63e8d56dc0438aa02b84496faabf4
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-13 14:10:09 +0900

    refactor : DTO를 통한 응답으로 변경

--- src/main/java/dailymissionproject/demo/domain/image/controller/ImageController.java
+++ src/main/java/dailymissionproject/demo/domain/image/controller/ImageController.java
@@ -1,9 +1,12 @@
 package dailymissionproject.demo.domain.image.controller;
 
+import dailymissionproject.demo.common.config.response.GlobalResponse;
 import dailymissionproject.demo.domain.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static dailymissionproject.demo.common.config.response.GlobalResponse.success;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,9 +14,8 @@ public class ImageController {
 
     private final ImageService imageService;
 
-    @GetMapping("/presigned-url/put")
-    public ResponseEntity<String> getPutPresignedUrl(@RequestParam String fileName, @RequestParam String title) {
-        String presignedUrl = String.valueOf(imageService.generatePostPresignedUrl(fileName, title));
-        return ResponseEntity.ok(presignedUrl);
+    @GetMapping("/api/v1/image/presigned-url")
+    public ResponseEntity<GlobalResponse> getPutPresignedUrl(@RequestParam String fileName, @RequestParam String title) {
+        return ResponseEntity.ok(success(imageService.generatePostPresignedUrl(fileName, title)));
     }
 }
