0545d15d3f573c665fcb43b7e1e3bd0ba22e9c13
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-22 10:58:40 +0900

    feat : save 쿼리 추가

--- src/main/java/dailymissionproject/demo/domain/like/repository/LikeRepositoryImpl.java
+++ src/main/java/dailymissionproject/demo/domain/like/repository/LikeRepositoryImpl.java
@@ -1,12 +1,11 @@
 package dailymissionproject.demo.domain.like.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface LikeRepositoryImpl extends LikeRepository, JpaRepository<Likes, Long> {
+public interface LikeRepositoryImpl extends LikesRepository, JpaRepository<Likes, Long> {
 
     @Override
-    public Optional<Likes> findByPostIdAndUserId(Long postId, Long userId);
+    Optional<Likes> findByPostIdAndUserId(Long postId, Long userId);
 }
--- src/main/java/dailymissionproject/demo/domain/like/repository/LikesRepository.java
+++ src/main/java/dailymissionproject/demo/domain/like/repository/LikesRepository.java
@@ -5,5 +5,6 @@ import java.util.Optional;
 public interface LikesRepository {
 
     Optional<Likes> findByPostIdAndUserId(Long postId, Long userId);
+
     Long save(Likes likes);
 }
