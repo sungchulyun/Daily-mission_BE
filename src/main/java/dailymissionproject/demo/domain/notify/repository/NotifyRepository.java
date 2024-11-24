7468e0a932f22033cd8365bd787368ad83ee5c30
sungchulyun <proattacker@naver.com>
2024-11-24 22:21:32 +0900

    feat : notification repository 작성

---
+++ src/main/java/dailymissionproject/demo/domain/notify/repository/NotifyRepository.java
@@ -0,0 +1,6 @@
+package dailymissionproject.demo.domain.notify.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotifyRepository extends JpaRepository<Notification, Long> {
+}
