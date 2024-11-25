857e795d9ea98b716f679b073b29910c904a856d
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-25 13:18:10 +0900

    feat : sse 연결 요청 api 작성

--- src/main/java/dailymissionproject/demo/domain/notify/controller/NotifyController.java
+++ src/main/java/dailymissionproject/demo/domain/notify/controller/NotifyController.java
@@ -10,7 +10,9 @@ import dailymissionproject.demo.domain.user.repository.User;
 import dailymissionproject.demo.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -23,11 +25,8 @@ public class NotifyController {
     private final NotifyService notifyService;
     private final UserRepository userRepository;
 
-    @GetMapping(value = "subscribe/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@CurrentUser CustomOAuth2User user) {
-        User findUser = userRepository.findById(user.getId())
-                .orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
-
-        return notifyService.subscribe(findUser.getId());
+        return notifyService.subscribe(user.getId());
     }
 }
