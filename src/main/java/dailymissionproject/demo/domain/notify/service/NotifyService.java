26b038a639ef2572240e62dc64a05648464eae5e
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-25 13:18:42 +0900

    feat : 알림 전송 메서드 작성

--- src/main/java/dailymissionproject/demo/domain/notify/service/NotifyService.java
+++ src/main/java/dailymissionproject/demo/domain/notify/service/NotifyService.java
@@ -1,20 +1,29 @@
 package dailymissionproject.demo.domain.notify.service;
 
+import dailymissionproject.demo.domain.notify.dto.NotifyDto;
 import dailymissionproject.demo.domain.notify.repository.EmitterRepositoryImpl;
 import dailymissionproject.demo.domain.notify.repository.Notification;
 import dailymissionproject.demo.domain.notify.repository.NotificationType;
 import dailymissionproject.demo.domain.notify.repository.NotifyRepository;
+import dailymissionproject.demo.domain.user.exception.UserException;
+import dailymissionproject.demo.domain.user.exception.UserExceptionCode;
 import dailymissionproject.demo.domain.user.repository.User;
 import dailymissionproject.demo.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.ast.Not;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NotifyService {
 
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
@@ -25,35 +34,52 @@ public class NotifyService {
 
     //Emitter 생성
     public SseEmitter subscribe(Long userId){
+        User findUser = userRepository.findById(userId).orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
         SseEmitter emitter = create(userId);
 
-        sendToClient(userId, "Event stream is created. [userId : " +  userId + "]");
+        try {
+            emitter.send(SseEmitter.event()
+                    .id(String.valueOf(userId))
+                    .data("Event created. [userId = " + userId + "]"));
+        } catch (IOException e) {
+            throw new RuntimeException("전송에 실패했습니다.");
+        }
+
         return emitter;
     }
 
-    public void notify(User receiver, NotificationType notificationType, Object event){
+    public void send(User receiver, NotificationType notificationType, Object event){
         Notification notification = Notification.builder()
                 .receiver(receiver)
                 .content(String.valueOf(event))
                 .notificationType(notificationType)
                 .build();
 
-        sendToClient(receiver.getId(), event);
+        SseEmitter emitter = emitterRepository.get(receiver.getId());
+
+        if (Objects.isNull(emitter)) {
+            log.warn("연결중이지 않은 userId={}", receiver.getId());
+        }
+
+        NotifyDto response = NotifyDto.builder()
+                .id(receiver.getId())
+                .content(String.valueOf(event))
+                .type(notificationType)
+                .build();
+
+        sendNotification(emitter, response);
         notifyRepository.save(notification);
     }
 
-    private void sendToClient(Long userId, Object data) {
-        SseEmitter emitter = emitterRepository.get(userId);
-
-        if(!Objects.isNull(emitter)){
-            try {
-                emitter.send(data);
-            } catch (IOException e) {
-                throw new RuntimeException("연결 오류!");
-            }
+    private void sendNotification(SseEmitter emitter, NotifyDto response) {
+        try {
+            emitter.send(SseEmitter.event()
+                    .id(String.valueOf(response.getId()))
+                    .data(response));
+        } catch (IOException e) {
+            throw new RuntimeException("전송에 실패했습니다.");
         }
     }
-
 
     private SseEmitter create(Long userId){
         SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
