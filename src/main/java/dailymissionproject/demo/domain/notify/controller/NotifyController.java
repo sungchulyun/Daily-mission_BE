e0581bf99e324dfdb1df718f8cbd9c67a741290f
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-26 16:52:28 +0900

    refactor : delete user repository dependency

--- src/main/java/dailymissionproject/demo/domain/notify/controller/NotifyController.java
+++ src/main/java/dailymissionproject/demo/domain/notify/controller/NotifyController.java
@@ -3,16 +3,11 @@ package dailymissionproject.demo.domain.notify.controller;
 
 import dailymissionproject.demo.common.repository.CurrentUser;
 import dailymissionproject.demo.domain.auth.dto.CustomOAuth2User;
-import dailymissionproject.demo.domain.notify.service.NotifyService;
-import dailymissionproject.demo.domain.user.exception.UserException;
-import dailymissionproject.demo.domain.user.exception.UserExceptionCode;
-import dailymissionproject.demo.domain.user.repository.User;
+import dailymissionproject.demo.domain.notify.service.EmitterService;
 import dailymissionproject.demo.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -22,11 +17,10 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequestMapping("/api/v1/notify")
 public class NotifyController {
 
-    private final NotifyService notifyService;
-    private final UserRepository userRepository;
+    private final EmitterService emitterService;
 
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@CurrentUser CustomOAuth2User user) {
-        return notifyService.subscribe(user.getId());
+        return emitterService.subscribe(user.getId());
     }
 }
