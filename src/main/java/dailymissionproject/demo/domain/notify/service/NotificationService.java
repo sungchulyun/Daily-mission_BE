d9ae28844aa2249618e0102c097e28b4847d3151
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-27 13:49:06 +0900

    feat : 유저 검증 로직 작성

--- src/main/java/dailymissionproject/demo/domain/notify/service/NotificationService.java
+++ src/main/java/dailymissionproject/demo/domain/notify/service/NotificationService.java
@@ -4,9 +4,13 @@ import dailymissionproject.demo.domain.auth.dto.CustomOAuth2User;
 import dailymissionproject.demo.domain.mission.dto.page.PageResponseDto;
 import dailymissionproject.demo.domain.notify.dto.NotifyDto;
 import dailymissionproject.demo.domain.notify.dto.response.UserNotifyResponseDto;
+import dailymissionproject.demo.domain.notify.repository.Notification;
 import dailymissionproject.demo.domain.notify.repository.NotificationRepository;
 import dailymissionproject.demo.domain.notify.repository.NotificationType;
+import dailymissionproject.demo.domain.user.exception.UserException;
+import dailymissionproject.demo.domain.user.exception.UserExceptionCode;
 import dailymissionproject.demo.domain.user.repository.User;
+import dailymissionproject.demo.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +26,7 @@ public class NotificationService {
 
     private final KafkaTemplate<String, NotifyDto> kafkaTemplate;
     private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
 
     public void createNotification(User receiver, NotificationType notificationType, Object event){
         NotifyDto notifyDto = NotifyDto.builder()
@@ -35,8 +40,12 @@ public class NotificationService {
 
     @Transactional(readOnly = true)
     public PageResponseDto getUserNotifications(CustomOAuth2User user, Pageable pageable){
-        Slice<UserNotifyResponseDto> responses = notificationRepository.findUnreadNotificationByUserId(user.getId(), pageable);
+        User findUser = userRepository.findById(user.getId()).orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
+
+        Slice<UserNotifyResponseDto> responses = notificationRepository.findUnreadNotificationByUserId(findUser.getId(), pageable);
 
         return new PageResponseDto(responses.getContent(), responses.hasNext());
     }
+
+
 }
