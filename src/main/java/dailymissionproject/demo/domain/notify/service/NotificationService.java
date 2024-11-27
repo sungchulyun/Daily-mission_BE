73164bed0830ba6c104fb0b1a4a9dad65d968c48
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-11-27 13:44:12 +0900

    feat : 유저별 알림 목록 조회 service 메서드 작성

--- src/main/java/dailymissionproject/demo/domain/notify/service/NotificationService.java
+++ src/main/java/dailymissionproject/demo/domain/notify/service/NotificationService.java
@@ -1,18 +1,27 @@
 package dailymissionproject.demo.domain.notify.service;
 
+import dailymissionproject.demo.domain.auth.dto.CustomOAuth2User;
+import dailymissionproject.demo.domain.mission.dto.page.PageResponseDto;
 import dailymissionproject.demo.domain.notify.dto.NotifyDto;
+import dailymissionproject.demo.domain.notify.dto.response.UserNotifyResponseDto;
+import dailymissionproject.demo.domain.notify.repository.NotificationRepository;
 import dailymissionproject.demo.domain.notify.repository.NotificationType;
 import dailymissionproject.demo.domain.user.repository.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class NotificationService {
+
     private final KafkaTemplate<String, NotifyDto> kafkaTemplate;
+    private final NotificationRepository notificationRepository;
 
     public void createNotification(User receiver, NotificationType notificationType, Object event){
         NotifyDto notifyDto = NotifyDto.builder()
@@ -22,5 +31,12 @@ public class NotificationService {
                 .build();
 
         kafkaTemplate.send("notify", notifyDto);
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponseDto getUserNotifications(CustomOAuth2User user, Pageable pageable){
+        Slice<UserNotifyResponseDto> responses = notificationRepository.findUnreadNotificationByUserId(user.getId(), pageable);
+
+        return new PageResponseDto(responses.getContent(), responses.hasNext());
     }
 }
