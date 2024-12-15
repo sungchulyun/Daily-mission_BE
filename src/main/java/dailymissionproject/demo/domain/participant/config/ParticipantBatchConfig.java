0c7b7bdca633a44d7820579ab040917c467b0620
sungchulyun <proattacker@naver.com>
2024-12-15 19:12:12 +0900

    refactor : 인증글 제출 검증 로직 작성

--- src/main/java/dailymissionproject/demo/domain/participant/config/ParticipantBatchConfig.java
+++ src/main/java/dailymissionproject/demo/domain/participant/config/ParticipantBatchConfig.java
@@ -4,9 +4,7 @@ package dailymissionproject.demo.domain.participant.config;
 import dailymissionproject.demo.domain.mission.repository.Mission;
 import dailymissionproject.demo.domain.mission.repository.MissionRepository;
 import dailymissionproject.demo.domain.participant.repository.Participant;
-import dailymissionproject.demo.domain.participant.repository.ParticipantRepository;
 import dailymissionproject.demo.domain.post.repository.PostRepository;
-import jakarta.servlet.http.Part;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -14,13 +12,9 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.RepositoryItemReader;
-import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
-import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
@@ -51,51 +45,6 @@ public class ParticipantBatchConfig {
                 .start(banStep())
                 .build();
     }
-
-    /*
-
-    @Bean
-    public Step banStep() {
-        return new StepBuilder("banStep", jobRepository)
-                .<Participant, Participant> chunk(10, platformTransactionManager)
-                .reader(banBeforeReader())
-                .processor(banProcessor())
-                .writer(banAfterWriter())
-                .listener(writerListener)
-                .build();
-    }
-
-    @Bean
-    public RepositoryItemReader<Participant> banBeforeReader(){
-        return new RepositoryItemReaderBuilder<Participant>()
-                .name("beforeReader")
-                .pageSize(chunkSize)
-                .methodName("findAllAndBannedIsFalse")
-                .repository(participantRepository)
-                .sorts(Map.of("id", Sort.Direction.ASC))
-                .build();
-    }
-
-    @Bean
-    public ItemProcessor<Participant, Participant> banProcessor() {
-        return item -> {
-            if(!isSubmitToday(item, LocalDateTime.now())){
-                item.ban();
-            }
-
-            return item;
-        };
-    }
-
-   @Bean
-   public ItemWriter<Participant> banAfterWriter() {
-        return new RepositoryItemWriterBuilder<Participant>()
-                .repository(participantRepository)
-                .methodName("save")
-                .build();
-   }
-
-   */
 
     @Bean
     public Step banStep() {
@@ -130,9 +79,8 @@ public class ParticipantBatchConfig {
             }
 
             return mission.getParticipants().stream()
-                    .filter(participant -> isSubmitToday(participant, LocalDateTime.now()))
-                    .filter(participant -> !participant.isBanned()) // 아직 강퇴되지 않은 참여자만 처리
-                    .peek(Participant::ban) // 강퇴 상태로 변경
+                    .filter(participant -> !isSubmitToday(participant, LocalDateTime.now()))
+                    .peek(Participant::ban)
                     .toList();
         };
     }
@@ -142,15 +90,10 @@ public class ParticipantBatchConfig {
      * 설명 : 매일 03시에 강퇴 배치를 수행하기 때문에
      * 전날 03시 ~ 다음날 03시를 각각 분기 처리한다.
      */
-    public boolean isSubmitToday(Participant participant, LocalDateTime now){
+    private boolean isSubmitToday(Participant participant, LocalDateTime now){
         long postCount = 0;
         LocalDateTime criteria = LocalDate.now().atTime(03,00);
         if(now.isBefore(criteria)){
-//            // 전날 새벽 3시 ~ 현재
-//            isSubmit = postRepository.countPostSubmit(participant.getMission()
-//                    , participant.getUser()
-//                    ,criteria.minusDays(1)
-//                    , now) > 0;
 
             postCount = participant.getUser().getPosts().stream()
                     .filter(post -> post.getCreatedDate().isAfter(criteria.minusDays(1)))
@@ -158,12 +101,6 @@ public class ParticipantBatchConfig {
                     .count();
 
         } else {
-            // 금일 새벽 3시  ~ 현재
-//            isSubmit = postRepository.countPostSubmit(participant.getMission()
-//                    ,participant.getUser()
-//                    ,criteria
-//                    ,now) > 0;
-
             postCount = participant.getUser().getPosts().stream()
                     .filter(post -> post.getCreatedDate().isAfter(criteria))
                     .filter(post -> post.getCreatedDate().isBefore(now))
