063e744463f6157f49e2e9170958314f71090642
sungchulyun <proattacker@naver.com>
2024-12-15 20:45:08 +0900

    refactor : 인증글 제출 검증 로직 작성

--- src/main/java/dailymissionproject/demo/common/config/BatchSchedulerConfig.java
+++ src/main/java/dailymissionproject/demo/common/config/BatchSchedulerConfig.java
@@ -8,7 +8,6 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
