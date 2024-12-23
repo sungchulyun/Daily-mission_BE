0d8a9e053fdbd193decb97cb38cf5a63824e90a9
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-12-23 17:45:47 +0900

    feat : pub/sub 로직 작성

---
+++ src/main/java/dailymissionproject/demo/domain/notify/service/RedisMessageService.java
@@ -0,0 +1,36 @@
+package dailymissionproject.demo.domain.notify.service;
+
+
+import dailymissionproject.demo.domain.notify.dto.NotifyDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Service;
+
+import java.nio.channels.Channel;
+
+@RequiredArgsConstructor
+@Service
+public class RedisMessageService {
+
+    private final RedisMessageListenerContainer container;
+    private final RedisSubscriber subscriber;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void subscribe(String channel) {
+        container.addMessageListener(subscriber, ChannelTopic.of(getChannelName(channel)));
+    }
+
+    public void publish(String channel, NotifyDto notifyDto){
+        redisTemplate.convertAndSend(getChannelName(channel), notifyDto);
+    }
+
+    public void removeSubscribe(String channel){
+        container.removeMessageListener(subscriber, ChannelTopic.of(getChannelName(channel)));
+    }
+
+    private String getChannelName(String id){
+        return String.valueOf(id);
+    }
+}
