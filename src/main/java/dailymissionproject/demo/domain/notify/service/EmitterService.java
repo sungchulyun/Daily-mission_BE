a55172b4cfe9182b1e91a70e3610357e97bb700d
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-12-24 10:52:41 +0900

    refactor : emitter 제외한 의존성 제거

--- src/main/java/dailymissionproject/demo/domain/notify/service/EmitterService.java
+++ src/main/java/dailymissionproject/demo/domain/notify/service/EmitterService.java
@@ -23,23 +23,17 @@ import java.util.Objects;
 public class EmitterService {
 
     private final EmitterRepositoryImpl emitterRepository;
-    private final NotificationRepository notificationRepository;
     private final UserRepository userRepository;
 
     private final static String NOTIFY_NAME = "notify";
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
-    private final RedisMessageService redisMessageService;
 
-    public SseEmitter subscribe(Long userId){
-        User findUser = userRepository.findById(userId).orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
-        SseEmitter emitter = create(userId);
-
+    public SseEmitter subscribe(SseEmitter emitter, Long userId){
         try {
             emitter.send(SseEmitter.event()
                     .id(String.valueOf(userId))
                     .name(NOTIFY_NAME)
                     .data("Event created. [userId : " + userId + "]"));
-            redisMessageService.subscribe(String.valueOf(userId));
         } catch (IOException e) {
             throw new RuntimeException("전송에 실패했습니다.");
         }
@@ -47,29 +41,16 @@ public class EmitterService {
         return emitter;
     }
 
-    @Transactional
     public void send(NotifyDto request){
         User user = userRepository.findById(request.getId()).orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
 
         SseEmitter emitter = emitterRepository.get(request.getId());
-
-        Notification notification = Notification.builder()
-                .receiver(user)
-                .content(request.getContent())
-                .notificationType(request.getType())
-                .build();
-
-        if (Objects.isNull(emitter)) {
-            log.warn("구독중이지 않은 User : {}", request.getId());
-            notificationRepository.save(notification);
-            return;
+        if(emitter != null){
+            sendNotificationToClient(emitter, request);
         }
-
-        sendNotification(emitter, request);
-        notificationRepository.save(notification);
     }
 
-    private void sendNotification(SseEmitter emitter, NotifyDto response) {
+    private void sendNotificationToClient(SseEmitter emitter, NotifyDto response) {
         try {
             emitter.send(SseEmitter.event()
                     .id(String.valueOf(response.getId()))
@@ -80,19 +61,14 @@ public class EmitterService {
         }
     }
 
-    private SseEmitter create(Long userId){
+    public SseEmitter create(Long userId){
         SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
         emitterRepository.save(userId, emitter);
-
-        redisMessageService.subscribe(String.valueOf(userId));
-        emitter.onCompletion(() -> disconnect(userId));
-        emitter.onTimeout(() -> disconnect(userId));
 
         return emitter;
     }
 
-    private void disconnect(Long userId){
+    public void deleteEmitter(Long userId){
         emitterRepository.delete(userId);
-        redisMessageService.removeSubscribe(String.valueOf(userId));
     }
 }
