10d24efdbbb70a0df1ca58aa3951c98e1d9e2290
sungchul yoon <97434717+sungchulyun@users.noreply.github.com>
2024-12-27 15:25:02 +0900

    feat : 좋아요 controller 작성

--- src/main/java/dailymissionproject/demo/domain/like/controller/LikesController.java
+++ src/main/java/dailymissionproject/demo/domain/like/controller/LikesController.java
@@ -9,16 +9,14 @@ import dailymissionproject.demo.domain.user.exception.UserException;
 import dailymissionproject.demo.domain.user.exception.UserExceptionCode;
 import dailymissionproject.demo.domain.user.repository.User;
 import dailymissionproject.demo.domain.user.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static dailymissionproject.demo.common.config.response.GlobalResponse.success;
 
@@ -29,25 +27,18 @@ import static dailymissionproject.demo.common.config.response.GlobalResponse.suc
 public class LikesController {
 
     private final LikesService likesService;
-    private final UserRepository userRepository;
 
     @PreAuthorize("hasRole('ROLE_USER')")
-    @PostMapping("/save/{postId}")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "성공!"),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
-            @ApiResponse(responseCode = "404", description = "해당 포스트가 존재하지 않습니다."),
-            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
-    })
-    public ResponseEntity<GlobalResponse> save(@PathVariable Long postId, @CurrentUser CustomOAuth2User user) {
-        User findUser = userRepository.findById(user.getId())
-                .orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
+    @PostMapping("/{postId}")
+    @Operation(summary = "좋아요 추가", description = "사용자가 포스트에 대한 좋아요를 추가하고 싶을 때 사용하는 API입니다.")
+    public ResponseEntity<GlobalResponse> add(@PathVariable Long postId, @CurrentUser CustomOAuth2User user) {
+        return ResponseEntity.ok(success(likesService.addLike(postId, user.getId())));
+    }
 
-        LikesRequestDto likesRequest = LikesRequestDto.builder()
-                .postId(postId)
-                .userId(findUser.getId())
-                .build();
-
-        return ResponseEntity.ok(success(likesService.save(likesRequest)));
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @DeleteMapping("/{postId}")
+    @Operation(summary = "좋아요 취소", description = "사용자가 포스트에 대한 좋아요를 취소하고 싶을 때 사용하는 API입니다.")
+    public ResponseEntity<GlobalResponse> delete(@PathVariable Long postId, @CurrentUser CustomOAuth2User user) {
+        return ResponseEntity.ok(success(likesService.removeLike(postId, user.getId())));
     }
 }
