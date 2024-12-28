4546eba5640b7c7530b49b8b4142d5896f077da2
sungchulyun <proattacker@naver.com>
2024-12-28 22:43:43 +0900

    refactor : 좋아요 추가/취소 서비스 리팩토링

--- src/main/java/dailymissionproject/demo/domain/like/service/LikesService.java
+++ src/main/java/dailymissionproject/demo/domain/like/service/LikesService.java
@@ -1,26 +1,18 @@
 package dailymissionproject.demo.domain.like.service;
 
-import dailymissionproject.demo.domain.like.dto.request.LikesRequestDto;
 import dailymissionproject.demo.domain.like.dto.response.LikesResponseDto;
-import dailymissionproject.demo.domain.like.exception.LikesException;
-import dailymissionproject.demo.domain.like.exception.LikesExceptionCode;
 import dailymissionproject.demo.domain.like.repository.Likes;
 import dailymissionproject.demo.domain.like.repository.LikesRepository;
 import dailymissionproject.demo.domain.post.exception.PostException;
-import dailymissionproject.demo.domain.post.exception.PostExceptionCode;
 import dailymissionproject.demo.domain.post.repository.Post;
 import dailymissionproject.demo.domain.post.repository.PostRepository;
 import dailymissionproject.demo.domain.user.exception.UserException;
-import dailymissionproject.demo.domain.user.exception.UserExceptionCode;
 import dailymissionproject.demo.domain.user.repository.User;
 import dailymissionproject.demo.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-import static dailymissionproject.demo.domain.like.exception.LikesExceptionCode.INVALID_LIKE_REQUEST;
 import static dailymissionproject.demo.domain.post.exception.PostExceptionCode.POST_NOT_FOUND;
 import static dailymissionproject.demo.domain.user.exception.UserExceptionCode.USER_NOT_FOUND;
 
@@ -32,43 +24,53 @@ public class LikesService {
     private final UserRepository userRepository;
     private final LikesRepository likesRepository;
 
+    /**
+     * 좋아요를 누른 히스토리가 있을 경우 -> deleteLike 수행
+     * 좋아요 히스토리가 없을 경우 -> addLike 수행
+     * @param postId
+     * @param userId
+     * @return
+     */
     @Transactional
-    public LikesResponseDto addLike(Long postId, Long userId) {
+    public LikesResponseDto likePost(Long postId, Long userId) {
         User findUser = userRepository.findById(userId).orElseThrow(() -> new UserException(USER_NOT_FOUND));
         Post findPost = postRepository.findById(postId).orElseThrow(() -> new PostException(POST_NOT_FOUND));
 
-        boolean alreadyLiked = likesRepository.existsByPostIdAndUserId(postId, userId);
-        if(alreadyLiked) {
-            throw new LikesException(INVALID_LIKE_REQUEST);
-        }
-
-        Likes likes = Likes.builder()
-                .post(findPost)
-                .user(findUser)
-                .build();
-
-        findPost.incrementLikeCount();
-        likesRepository.save(likes);
+        boolean isLiked = likesRepository.findByPostIdAndUserId(postId, userId)
+                .map(likes -> {
+                    removeLike(findPost, likes);
+                    return false;
+                })
+                .orElseGet(() -> {
+                    addLike(findPost, findUser);
+                    return true;
+                });
 
         return LikesResponseDto.builder()
-                .likeId(likes.getId())
-                .userId(findUser.getId())
-                .postId(findPost.getId())
+                .postId(postId)
+                .userId(userId)
+                .isLiked(isLiked)
                 .build();
     }
 
-    @Transactional
-    public boolean removeLike(Long postId, Long userId) {
-        User findUser = userRepository.findById(userId).orElseThrow(() -> new UserException(USER_NOT_FOUND));
-        Post findPost = postRepository.findById(postId).orElseThrow(() -> new PostException(POST_NOT_FOUND));
+    private boolean addLike(Post post, User user) {
+        Likes likes = Likes.builder()
+                .post(post)
+                .user(user)
+                .build();
 
-        Optional<Likes> likes = likesRepository.findByPostIdAndUserId(postId, userId);
-        if(likes.isPresent()) {
-            likesRepository.delete(likes.get());
-            findPost.decrementLikeCount();
-            return true;
-        }
+        post.incrementLikeCount();
 
-        return false;
+        likesRepository.save(likes);
+
+        return true;
+    }
+
+    private boolean removeLike(Post post, Likes likes) {
+            post.decrementLikeCount();
+
+            likesRepository.delete(likes);
+
+            return false;
     }
 }
