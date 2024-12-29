0241c137a5337ff8f53480d851450005008b5f81
sungchulyun <proattacker@naver.com>
2024-12-29 23:10:53 +0900

    feat : 비관적 락 적용 조회 로직 작성

--- src/main/java/dailymissionproject/demo/domain/post/repository/PostRepository.java
+++ src/main/java/dailymissionproject/demo/domain/post/repository/PostRepository.java
@@ -1,15 +1,22 @@
 package dailymissionproject.demo.domain.post.repository;
 
-import dailymissionproject.demo.domain.mission.repository.Mission;
-import dailymissionproject.demo.domain.user.repository.User;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom{
 
       Post save(Post post);
 
+      @Lock(LockModeType.PESSIMISTIC_WRITE)
+      @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "5000")})
+      @Query("SELECT p FROM Post p WHERE p.id = :id")
+      Optional<Post> findWithPessimisticLockById(Long id);
 }
