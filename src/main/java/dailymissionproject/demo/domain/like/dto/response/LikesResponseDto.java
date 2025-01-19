deb0955ec1c4288ed8706b0c826336332ca0d856
sungchulyun <proattacker@naver.com>
2025-01-19 22:01:46 +0900

    docs : 좋아요 response swagger 작성

--- src/main/java/dailymissionproject/demo/domain/like/dto/response/LikesResponseDto.java
+++ src/main/java/dailymissionproject/demo/domain/like/dto/response/LikesResponseDto.java
@@ -1,17 +1,22 @@
 package dailymissionproject.demo.domain.like.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(force = true)
+@Schema(description = "좋아요 응답")
 public class LikesResponseDto {
 
+    @Schema(description = "포스트 ID")
     private final Long postId;
 
+    @Schema(description = "유저 ID")
     private final Long userId;
 
+    @Schema(description = "좋아요 여부")
     private final boolean isLiked;
 
     @Builder
