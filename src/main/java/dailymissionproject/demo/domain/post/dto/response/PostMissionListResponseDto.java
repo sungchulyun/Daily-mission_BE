45ff6ba0177bc54352d8cb672ad50f7283708172
sungchulyun <proattacker@naver.com>
2025-03-18 23:19:09 +0900

    refactor : liked로 필드명 수정

--- src/main/java/dailymissionproject/demo/domain/post/dto/response/PostMissionListResponseDto.java
+++ src/main/java/dailymissionproject/demo/domain/post/dto/response/PostMissionListResponseDto.java
@@ -37,7 +37,7 @@ public class PostMissionListResponseDto {
     @Schema(description = "포스트 좋아요")
     private final Long likeCount;
     @Schema(description = "해당 유저가 좋아요를 눌렀는지 여부")
-    private final boolean isLiked;
+    private final boolean liked;
 
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
@@ -51,7 +51,7 @@ public class PostMissionListResponseDto {
 
     @Builder
     public PostMissionListResponseDto(Long id, Long missionId, String nickname, String userImageUrl, String title
-            , String content, String imageUrl, Long likeCount, boolean isLiked
+            , String content, String imageUrl, Long likeCount, boolean liked
             , LocalDateTime createdDate, LocalDateTime modifiedDate) {
         this.id = id;
         this.missionId = missionId;
@@ -61,7 +61,7 @@ public class PostMissionListResponseDto {
         this.content = content;
         this.imageUrl = imageUrl;
         this.likeCount = likeCount;
-        this.isLiked = isLiked;
+        this.liked = liked;
         this.createdDate = createdDate;
         this.modifiedDate = modifiedDate;
     }
